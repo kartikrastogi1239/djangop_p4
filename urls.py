eb7117fd8287655be305d97e1bf19d7e030377cb
kartikrastogi1239 <84902928+kartikrastogi1239@users.noreply.github.com>
2023-06-12 00:14:35 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from helloworldapp.views import hello_world
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',hello_world),
+]
