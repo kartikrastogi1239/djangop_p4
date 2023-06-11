d9ae6193bde6b38681a865bbe26cdbe53d30963d
kartikrastogi1239 <84902928+kartikrastogi1239@users.noreply.github.com>
2023-06-12 00:15:28 +0530

    helloworldapp

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from helloworldapp.views import hello_world
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',hello_world),
-]
